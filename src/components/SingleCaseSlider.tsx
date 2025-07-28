6a861d83be1a777b5de2412908cfd15a665a8684
Maruthi-Bebo <maruthiboffc@gmail.com>
2025-07-28 21:13:50 +0530

    slider progress

--- src/components/SingleCaseSlider.tsx
+++ src/components/SingleCaseSlider.tsx
@@ -67,7 +67,7 @@ export default function SingleCaseSlider(props: SingleCaseSliderProps) {
       trigger: container,
       start: 'top bottom',
       end: 'bottom top',
-      toggleActions: 'pause pause resume pause',
+      toggleActions: 'play pause resume pause',
     }
   });
 
@@ -183,26 +183,28 @@ export default function SingleCaseSlider(props: SingleCaseSliderProps) {
   const images = props.caseItem?.images || [];
 
   return (
-    <div
-      ref={containerRef}
-      className="singleCaseSlider pt-[10rem] overflow-x-scroll scrollbar-hidden px-[5.4rem] select-none"
-    >
-      <ul ref={listRef} className="flex gap-[1.6rem] w-max relative mb-[4rem]">
-        <div className="h-full w-full absolute transparent z-[1] top-0 left-0"></div>
-        {images.concat(images).map((image, idx) => (
-          <li key={idx} className="min-w-[37.6rem]">
-            <img
-              loading="lazy"
-              alt={props.caseItem?.heading}
-              src={image.image.image?.desktopImage?.imageUrl}
-              width={376}
-              height={474}
-              className="w-[37.6rem] h-[47.4rem] object-cover"
-            />
-          </li>
-        ))}
-      </ul>
-      <div>
+    <div>
+      <div
+        ref={containerRef}
+        className="singleCaseSlider pt-[10rem] overflow-x-scroll scrollbar-hidden px-[5.4rem] select-none"
+      >
+        <ul ref={listRef} className="flex gap-[1.6rem] w-max relative mb-[4rem]">
+          <div className="h-full w-full absolute transparent z-[1] top-0 left-0"></div>
+          {images.concat(images).map((image, idx) => (
+            <li key={idx} className="min-w-[37.6rem]">
+              <img
+                loading="lazy"
+                alt={props.caseItem?.heading}
+                src={image.image.image?.desktopImage?.imageUrl}
+                width={376}
+                height={474}
+                className="w-[37.6rem] h-[47.4rem] object-cover"
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="px-[5.4rem]">
         <div className="">
           <p className="text-[4rem] font-[500] font-generalSans text-box-trim mb-[2rem]">{props.caseItem?.heading}</p>
           <p className="text-[2.2rem] font-[400] font-generalSans leading-[2.8rem] mb-[1rem]">{props.caseItem?.caption}</p>
